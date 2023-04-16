7fa6d438d7a15b1165a418a18e3019f2c6920892
ãjsskcr321 <jsskcr321@naver.com>
2023-04-16 18:26:30 +0900

    rq를 도입í

--- src/main/java/org/example/GugudanServlet.java
+++ src/main/java/org/example/GugudanServlet.java
@@ -16,15 +16,14 @@ public class GugudanServlet extends HttpServlet {
     resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML의 인코딩을 UTF-8로 하겠다.
     resp.setContentType("text/html; charset-utf-8"); //우리가 만든 결과물이 UTF-8이라고 알리는 의미.
 
-    int dan = Integer.parseInt(req.getParameter("dan"));
-    int limit = Integer.parseInt(req.getParameter("limit"));;
+    Rq rq = new Rq(req, resp);
 
-    resp.getWriter().append("<h1>%d단</h1>\n".formatted(dan));
+    int dan = rq.getIntParam("dan", 9);
+    int limit = rq.getIntParam("limit", 9);
+
+    rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
     for (int i = 1 ; i <= limit ; i++){
-      resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan, i ,dan*i));
+      rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i ,dan*i));
     }
-
-
-
   }
 }
